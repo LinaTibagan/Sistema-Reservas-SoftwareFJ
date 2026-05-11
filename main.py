5f3ad17a953beeded8cda02d2b92f06edcb3396f
Lina Mayerli <lmtibaganm@unadvirtual.edu.co>
2026-05-10 19:09:27 -0500

    modificacion en archivo main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from cliente import Cliente
+from excepciones import ClienteError
 
 try:
-    cliente1 = Cliente("Natalia", 123, "3000000000")
+
+    cliente1 = Cliente("Natalia", 123456, "3000000000")
+
     print(cliente1.mostrar_datos())
 
     cliente2 = Cliente("", 456, "3111111111")
 
-except ValueError as e:
+except ClienteError as e:
+
     print("Error:", e)
